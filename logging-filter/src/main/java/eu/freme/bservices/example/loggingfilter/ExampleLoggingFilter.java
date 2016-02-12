29b282bcfc430f928c516230cf61921866f653eb
jnehring <gmnoname@gmx.de>
2016-02-12 14:32:43 +0100

    add logging filter as example for basic service

---
+++ logging-filter/src/main/java/eu/freme/bservices/example/loggingfilter/ExampleLoggingFilter.java
@@ -0,0 +1,32 @@
+
+package eu.freme.bservices.example.loggingfilter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+@Component
+public class ExampleLoggingFilter extends GenericFilterBean{
+	
+	Logger logger = Logger.getLogger(ExampleLoggingFilter.class);
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res,
+			 FilterChain chain) throws IOException, ServletException {
+		
+		if( req instanceof HttpServletRequest){
+			HttpServletRequest httpRequest = (HttpServletRequest)req;
+			logger.info("Detect HTTP request to endpoint " + httpRequest.getRequestURI());
+		}
+		
+		chain.doFilter(req, res);
+	}
+}
